5045760772bf741a8a8d1a63d3d45afee31345d2
Gabriel Pereira <gabrielrcpereira@gmail.com>
2019-04-20 12:46:04 -0300

    Refactoring the JS functions

--- js/form.js
+++ js/form.js
@@ -6,7 +6,19 @@ document.querySelector("#adicionar-paciente")
 
         var nome = formAdiciona.nome.value;
         var peso = formAdiciona.peso.value;
+
+        if(isWeightValid(peso)){
+            alert("Peso inválido !");
+            return;
+        }
+
         var altura = formAdiciona.altura.value;
+        
+        if(isStatureValid(altura)){
+            alert("Altura inválida !");
+            return;
+        }
+
         var gordura = formAdiciona.gordura.value;
 
         var tabelaPacientes = document.querySelector("#tabela-pacientes");
@@ -18,10 +30,18 @@ document.querySelector("#adicionar-paciente")
         pacienteTr.appendChild(createElementTd(peso, "info-peso"));
         pacienteTr.appendChild(createElementTd(altura, "info-altura"));
         pacienteTr.appendChild(createElementTd(gordura, "info-gordura"));
-        pacienteTr.appendChild(createElementTd("", "info-imc"));
+        pacienteTr.appendChild(createElementTd(estimateImc(peso, altura), "info-imc"));
 
         tabelaPacientes.appendChild(pacienteTr);
 });
+
+function isStatureValid(altura) {
+    return altura <= 0 || altura >= 3;
+}
+
+function isWeightValid(peso) {
+    return peso <= 0 || peso > 1000;
+}
 
 function createElementTd(valueElement, classElement){
     var elementTd = document.createElement("td");
--- js/main.js
+++ js/main.js
@@ -4,34 +4,19 @@ titulo.textContent = "Aparecida Nutricionista";
 var pacientes = document.querySelectorAll(".paciente");
 pacientes.forEach(setImc);
 
-function setImc(paciente){
-    var tdPeso = paciente.querySelector(".info-peso");
-    var peso = tdPeso.textContent;
+function setImc(patientTr){
+    var tdPeso = patientTr.querySelector(".info-peso");
+    var weight = tdPeso.textContent;
+       
+    var tdAltura = patientTr.querySelector(".info-altura");
+    var stature = tdAltura.textContent;
     
-    var pesoEhValido = true;
-    var alturaEhValida = true;
+    var tdImc = patientTr.querySelector(".info-imc");
+    tdImc.textContent = estimateImc(weight, stature);
+}
+
+function estimateImc(weight, stature){
+    var imc = weight / ( stature * stature);    
     
-    var tdImc = paciente.querySelector(".info-imc");
-    
-    if(peso <= 0 || peso > 1000){
-        console.log("Peso inválido");
-        tdImc.textContent = "Peso inválido!";
-        pesoEhValido = false;
-    }
-    
-    var tdAltura = paciente.querySelector(".info-altura");
-    var altura = tdAltura.textContent;
-        
-    if(altura <= 0 || altura >= 3){
-        console.log("Altura inválida");
-        tdImc.textContent = "Altura inválida!";
-        alturaEhValida = false;
-    }
-    
-    if(pesoEhValido && alturaEhValida){
-        var imc = peso / ( altura * altura);    
-        tdImc.textContent = imc.toFixed(2);
-    } else {
-        paciente.classList.add("dados-invalidos");
-    }
+    return imc.toFixed(2);
 }
